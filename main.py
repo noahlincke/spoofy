1c83490d9c42a70118f2c5ec327362ee57aeda74
Noah Lincke <noah@lincke.org>
2019-07-16 23:34:13 -0700

    fixed about page link

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+
 @app.route('/spoof', methods=['POST'])
 def spoof():
     print("spoof")
