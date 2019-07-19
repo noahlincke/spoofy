3068bab33930417f72b53ac6d9d19cba3e9e6fbd
Noah Lincke <noah@lincke.org>
2019-07-18 19:36:55 -0700

    moved duration to js

--- static/js/form.js
+++ static/js/form.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
   $("form").on("submit", function(event) {
-    $.ajax({
-      url: "/spoof",
-      data: {
-        username: $("#usernameInput").val(),
-        password: $("#passwordInput").val(),
-        latitude: $("#latitudeInput").val(),
-        longitude: $("#longitudeInput").val(),
-        duration: $("#durationInput").val()
-      },
-      type: "POST",
-      success: function(response) {
-        console.log(response);
-      }
-    });
-
+    counter = 0;
+    while (counter < 10) {
+      $.ajax({
+        url: "/spoof",
+        data: {
+          username: $("#usernameInput").val(),
+          password: $("#passwordInput").val(),
+          latitude: $("#latitudeInput").val(),
+          longitude: $("#longitudeInput").val(),
+          duration: $("#durationInput").val()
+        },
+        type: "POST",
+        success: function(response) {
+          console.log(response);
+        }
+      });
+      counter++;
+    }
     event.preventDefault();
   });
 });
