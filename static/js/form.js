3f4acd96ac81d4ae4ef9422770da7a301366f5c0
Noah Lincke <noah@lincke.org>
2019-07-20 12:20:47 -0700

    fixed js

--- static/js/form.js
+++ static/js/form.js
@@ -1,23 +1,20 @@
 $(document).ready(function() {
   $("form").on("submit", function(event) {
-    counter = 0;
-    while (counter < 10) {
-      $.ajax({
-        url: "/spoof",
-        data: {
-          username: $("#usernameInput").val(),
-          password: $("#passwordInput").val(),
-          latitude: $("#latitudeInput").val(),
-          longitude: $("#longitudeInput").val(),
-          duration: $("#durationInput").val()
-        },
-        type: "POST",
-        success: function(response) {
-          console.log(response);
-        }
-      });
-      counter++;
-    }
+    $.ajax({
+      url: "/spoof",
+      data: {
+        username: $("#usernameInput").val(),
+        password: $("#passwordInput").val(),
+        latitude: $("#latitudeInput").val(),
+        longitude: $("#longitudeInput").val(),
+        duration: $("#durationInput").val()
+      },
+      type: "POST",
+      success: function(response) {
+        console.log(response);
+      }
+    });
+    counter++;
     event.preventDefault();
   });
 });
